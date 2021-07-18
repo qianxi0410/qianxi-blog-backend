b0217955dd450dbadc9d50206c4f86e7b787b215
qianxi0410 <894871277@qq.com>
2021-07-18 16:44:44 +0800

    Add post model

---
+++ model/post.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+type Post struct {
+	ID          uint      `gorm:"primarykey" json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt   time.Time `gorm:"index" json:"deleted_at"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Pre         int       `json:"pre"`
+	Next        int       `json:"next"`
+	Url         string    `json:"url"`
+	Path        string    `json:"path"`
+	Tags        string    `json:"tags"`
+}
