6fd654a8a13dc059e79ae258f81aa960b89a25f2
qianxi0410 <894871277@qq.com>
2021-09-27 20:35:26 +0800

    Config mysql redis rpc

--- service/blog/api/internal/config/config.go
+++ service/blog/api/internal/config/config.go
@@ -1,7 +1,20 @@
 package config
 
-import "github.com/tal-tech/go-zero/rest"
+import (
+	"github.com/tal-tech/go-zero/core/stores/cache"
+	"github.com/tal-tech/go-zero/rest"
+	"github.com/tal-tech/go-zero/zrpc"
+)
 
 type Config struct {
+	// rest url
 	rest.RestConf
+	// $user:$password@tcp($ip:$port)/$db?$queries
+	Mysql struct {
+		DataSource string
+	}
+	// redis
+	CacheRedis cache.CacheConf
+	// rpc client
+	UserRpc    zrpc.RpcClientConf
 }
