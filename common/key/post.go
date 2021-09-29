9fee525c5888f0c79d4b5aeb203a0190b9b4bcfa
qianxi0410 <894871277@qq.com>
2021-09-29 17:34:55 +0800

    Chnage id type

--- common/key/post.go
+++ common/key/post.go
@@ -10,14 +10,14 @@ func PostsCountWithTag(tag string) string {
 	return fmt.Sprintf("%s_tag_count", tag)
 }
 
-func Post(id string) string {
-	return fmt.Sprintf("%s_post", id)
+func Post(id int64) string {
+	return fmt.Sprintf("%d_post", id)
 }
 
-func Posts(page, size int) string {
+func Posts(page, size int64) string {
 	return fmt.Sprintf("%d_%d_page_size", page, size)
 }
 
-func PostsWithTag(page, size int, tag string) string {
+func PostsWithTag(page, size int64, tag string) string {
 	return fmt.Sprintf("%d_%d_page_size_%s", page, size, tag)
 }
